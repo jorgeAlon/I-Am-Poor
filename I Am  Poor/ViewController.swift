3edb673dce487e9c5e8e3c82166f26eeeba1d458
jorgeAlon <alonsojorget@gmail.com>
2020-02-19 22:44:30 -0600

    Segunda app con IOS

---
+++ I Am  Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am  Poor
+//
+//  Created by Jorge Alonso on 09/01/20.
+//  Copyright © 2020 Jorge Alonso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
